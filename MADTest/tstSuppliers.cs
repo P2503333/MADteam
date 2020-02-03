91e59f75c40ce4732d4bc5818a96c09f4295882c
Thassar <P2502008@student.dmu.ac.uk>
2020-02-03 11:37:42 +0000

    Created a class and some tests for it

---
+++ MADTest/tstSuppliers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MADClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MADTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsSuppliers Supplier1 = new clsSuppliers();
+            Assert.IsNotNull(Supplier1);
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            clsSuppliers Supplier1 = new clsSuppliers();
+            Boolean TestData = true;
+            Supplier1.Active = TestData;
+            Assert.AreEqual(Supplier1.Active, TestData);
+        }
+    }
+}
