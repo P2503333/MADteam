adf80165c2214e1677c15ce723e580488c23217e
Monalisa Dinesh <P17165980@my365.dmu.ac.uk>
2020-03-02 11:47:11 +0000

    added AnOrder.aspx, AnOrder.aspx.cs, order viewer.aspx

--- MADFrontOffice/AnOrder.aspx.cs
+++ MADFrontOffice/AnOrder.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MADClasses;
 
 public partial class AnOrder : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void btnOK_Click(object sender, EventArgs e)
     {
+        // creat a new instance of clsAddress
+        clsOrder clsOrder = new clsOrder();
+
+        //capture the Order_ID 
+        AnOrder.Order_ID = txtOrder_ID.Text;
+
+        // store the address in the session Object 
+        Session["AnOrder"] = AnOrder;
+
+        //redirect to the viewer page 
+        Response.Redirect("Address Viewer.aspx");
 
     }
 }
