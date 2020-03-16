3cd03bdf888347bf004acdccfd749e58dd14160f
Thassar <P2502008@student.dmu.ac.uk>
2020-03-16 11:48:13 +0000

    Monday's work

---
+++ MADClasses/clsSupplierCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MADClasses
+{
+    public class clsSupplierCollection
+    {
+        List<clsSuppliers> mSupplierList = new List<clsSuppliers>();
+        public List<clsSuppliers> SupplierList {
+            get
+            {
+                return mSupplierList;
+            }
+            set
+            {
+                mSupplierList = value;
+            }
+        }
+        public clsSuppliers ThisSupplier { get; set; }
+        public int Count
+        {
+            get
+            {
+                return mSupplierList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+        public clsSupplierCollection()
+        {
+            clsSuppliers TestItem = new clsSuppliers();
+            TestItem.Active = true;
+            TestItem.Address = "123 WotC Lane";
+            TestItem.Email = "wotc@wotc.com";
+            TestItem.ID = 10;
+            TestItem.LastDelivery = DateTime.Now.Date;
+            TestItem.Name = "WotC";
+            mSupplierList.Add(TestItem);
+
+            TestItem = new clsSuppliers();
+            TestItem.Active = true;
+            TestItem.Address = "1 Konami Road";
+            TestItem.Email = "yu@gi.oh";
+            TestItem.ID = 11;
+            TestItem.LastDelivery = DateTime.Now.Date;
+            TestItem.Name = "Konami";
+            mSupplierList.Add(TestItem);
+
+        }
+
+    }
+}
