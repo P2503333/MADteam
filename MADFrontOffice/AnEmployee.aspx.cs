a41bfa9cc1d0bd3b5969940a9944020abfdb3e67
Thomas Wilson <P17213482@my365.dmu.ac.uk>
2020-03-20 21:57:01 +0000

    added the delete page and ive edited the all of the validation files.

--- MADFrontOffice/AnEmployee.aspx.cs
+++ MADFrontOffice/AnEmployee.aspx.cs
@@ -16,37 +16,114 @@ public partial class AnEmployee : System.Web.UI.Page
         ClsEmployee anEmployee = new ClsEmployee();
 
         anEmployee = (ClsEmployee)Session["anEmployee"];
-        Response.Write(anEmployee.Emp_ID);
+        if (IsPostBack == false)
+        {
+            DisplayEmployee();
+
+            if (emp_ID != -1)
+            {
+                DisplayEmployee();
+            }
+        }
     }
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        ClsEmployee anEmployee = new ClsEmployee();
-
-        anEmployee.Emp_ID = txtemp_ID.Text;
-        anEmployee.Emp_Name = txtemp_Name.Text;
-        anEmployee.Job_Name = txtjob_Name.Text;
-        anEmployee.Manager_ID = txtmanager_ID.Text;
-        anEmployee.Hire_Date = Convert.ToDateTime( txthire_Date.Text);
-        anEmployee.Salary = txtsalary.Text;
-        anEmployee.Dep_ID = txtdep_ID.Text;
-        anEmployee.Active = txtactive.Text;
-        Session["AnEmployee"] = anEmployee;
-
-        Response.Write("EmployeeViewer.aspx");
+        if (emp_ID == -1)
+        {
+            //add the new record
+            Add();
+        }
+        else
+        {
+            //update the record
+            Update();
+        }
     }
+
     void DisplayEmployee()
     {
-        ClsEmployee anEmployee= new ClsEmployee();
+        clsEmployeeCollection EmployeeList= new clsEmployeeCollection();
 
-        anEmployee.Find(emp_ID);
+        EmployeeList.ThisEmployee.Find(emp_ID);
         //display the data
-        txtemp_ID.Text = anEmployee.Emp_ID;
+
+        txtemp_ID.Text = EmployeeList.ThisEmployee.Emp_ID.ToString();
+        txtemp_Name.Text = EmployeeList.ThisEmployee.Emp_Name;
+        txtjob_Name.Text = EmployeeList.ThisEmployee.Job_Name;
+        txtmanager_ID.Text = EmployeeList.ThisEmployee.Manager_ID.ToString();
+        txthire_Date.Text = EmployeeList.ThisEmployee.Hire_Date.ToString();
+        txtsalary.Text = EmployeeList.ThisEmployee.Salary.ToString();
+        txtdep_ID.Text = EmployeeList.ThisEmployee.Dep_ID.ToString();
+        chkActive.Checked = EmployeeList.ThisEmployee.Active;
     }
 
     protected void btnCancel_Click(object sender, EventArgs e)
     {
         //redirect to the main page
         Response.Redirect("Default.aspx");
+    }
+
+    //function for adding new records
+    void Add()
+    {
+        //create an instance of the address book
+        clsEmployeeCollection EmployeeBook = new clsEmployeeCollection();
+        //validate the data on the web form
+        String Error = EmployeeBook.ThisEmployee.Valid(Convert.ToInt32(txtemp_ID.Text), txtemp_Name.Text, txtjob_Name.Text, Convert.ToInt32(txtmanager_ID.Text), Convert.ToDateTime(txthire_Date.Text), Convert.ToInt32(txtsalary.Text), Convert.ToInt32(txtdep_ID.Text), chkActive.Checked);
+        //if the data is OK then add it to the object
+        if (Error == "")
+        {
+            //get the data entered by the user
+            EmployeeBook.ThisEmployee.Emp_ID = Convert.ToInt32(txtemp_ID.Text);
+            EmployeeBook.ThisEmployee.Emp_Name = txtemp_Name.Text;
+            EmployeeBook.ThisEmployee.Job_Name = txtjob_Name.Text;
+            EmployeeBook.ThisEmployee.Manager_ID = Convert.ToInt32(txtmanager_ID.Text);
+            EmployeeBook.ThisEmployee.Hire_Date = Convert.ToDateTime(txthire_Date.Text);
+            EmployeeBook.ThisEmployee.Salary = Convert.ToInt32(txtsalary.Text);
+            EmployeeBook.ThisEmployee.Dep_ID = Convert.ToInt32(txtdep_ID.Text);
+            EmployeeBook.ThisEmployee.Active = chkActive.Checked;
+            //add the record
+            EmployeeBook.Add();
+            //all done so redirect back to the main page
+            Response.Redirect("Default.aspx");
+        }
+        else
+        {
+            //report an error
+            lblError.Text = "There were problems with the data entered " + Error;
+        }
+    }
+
+    void Update()
+    {
+        //create an instance of the address book
+        clsEmployeeCollection EmployeeBook = new clsEmployeeCollection();
+        //validate the data on the web form
+        String Error = EmployeeBook.ThisEmployee.Valid(Convert.ToInt32(txtemp_ID.Text), txtemp_Name.Text, txtjob_Name.Text, Convert.ToInt32(txtmanager_ID.Text), Convert.ToDateTime(txthire_Date.Text), Convert.ToInt32(txtsalary.Text), Convert.ToInt32(txtdep_ID.Text), chkActive.Checked);
+        //if the data is OK then add it to the object
+        if (Error == "")
+        {
+            //find the record to update
+            EmployeeBook.ThisEmployee.Find(emp_ID);
+            //get the data entered by the user
+            EmployeeBook.ThisEmployee.Emp_ID = Convert.ToInt32(txtemp_ID.Text);
+            EmployeeBook.ThisEmployee.Emp_Name = txtemp_Name.Text;
+            EmployeeBook.ThisEmployee.Job_Name = txtjob_Name.Text;
+            EmployeeBook.ThisEmployee.Manager_ID = Convert.ToInt32(txtmanager_ID.Text);
+            EmployeeBook.ThisEmployee.Hire_Date = Convert.ToDateTime(txthire_Date.Text);
+            EmployeeBook.ThisEmployee.Salary = Convert.ToInt32(txtsalary.Text);
+            EmployeeBook.ThisEmployee.Dep_ID = Convert.ToInt32(txtdep_ID.Text);
+            EmployeeBook.ThisEmployee.Active = chkActive.Checked; ;
+            //update the record
+            EmployeeBook.Update();
+            //all done so redirect back to the main page
+            Response.Redirect("Default.aspx");
+        }
+        else
+        {
+            //report an error
+            lblError.Text = "There were problems with the data entered " + Error;
+        }
     }
 }
