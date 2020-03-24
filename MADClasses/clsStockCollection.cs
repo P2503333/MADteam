9923de8f9f827a3705bb06e07a724e35ab04f577
Ryan Woolston <P2503333@my365.dmu.ac.uk>
2020-03-24 22:19:18 +0000

    Temporarily excluded several order components to get the build to work. Finished validate, began work on stock collection

---
+++ MADClasses/clsStockCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MADClasses
+{
+    public class clsStockCollection
+    {
+        public List<clsStock> StockList { get; set; }
+        public int Count { get; set; }
+        public clsStock ThisStock { get; set; }
+    }
+}
