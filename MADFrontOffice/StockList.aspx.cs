da1f3b4369f7709878cd7fa0cfa180228a480547
Ryan Woolston <P2503333@my365.dmu.ac.uk>
2020-03-27 16:06:38 +0000

    Update with a workaround to the c# version error

--- MADFrontOffice/StockList.aspx.cs
+++ MADFrontOffice/StockList.aspx.cs
@@ -9,12 +9,16 @@ public partial class StockList : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        /*if (IsPostBack == false)
+        if (IsPostBack == false)
         {
-            DisplayStock();
+            MADClasses.clsStockCollection Stock = new MADClasses.clsStockCollection();
+            lstStockList.DataSource = Stock.StockList;
+            lstStockList.DataValueField = "ISBN";
+            lstStockList.DataTextField = "BookName";
+            lstStockList.DataBind();
         }
 
-        void DisplayStock()
+        /*void DisplayStock()
         {
             MADClasses.clsStockCollection Stock = new MADClasses.clsStockCollection();
             lstStockList.DataSource = Stock.StockList;
